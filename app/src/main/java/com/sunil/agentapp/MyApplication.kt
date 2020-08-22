83028146956fec991b71233dc7fbb67c7107c989
Sunil Kumar Yadav <sunilkumar.yadav@rampgroup.com>
2020-08-22 10:10:12 +0530

    init mvvm project structure

---
+++ app/src/main/java/com/sunil/agentapp/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.sunil.agentapp
+
+import android.app.Application
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/sunil/agentapp/view/MainActivity.kt
+++ app/src/main/java/com/sunil/agentapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.sunil.agentapp
+package com.sunil.agentapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sunil.agentapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
